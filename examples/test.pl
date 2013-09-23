ca50de5a254143ee94dcbb1a70fe28f1116e7959
Mitch McCracken <mrmccrac@gmail.com>
2013-09-23 03:54:52 +0000

    Import of DEFCON/WebService-FileCloud-0.1 from CPAN.
    gitpan-cpan-distribution: WebService-FileCloud
    gitpan-cpan-version:      0.1
    gitpan-cpan-path:         DEFCON/WebService-FileCloud-0.1.tar.gz
    gitpan-cpan-author:       DEFCON
    gitpan-cpan-maturity:     released

---
+++ examples/test.pl
@@ -0,0 +1,30 @@
+#!perl
+
+use strict;
+use warnings;
+
+use WebService::FileCloud;
+use Data::Dumper;
+
+# update these values accordingly
+use constant AKEY => 'APIKEY';
+use constant UKEY => 'FILEKEY';
+use constant TKEY => 'TAGKEY';
+use constant USERNAME => 'USERNAME';
+use constant PASSWORD => 'PASSWORD';
+use constant UPLOAD_URL => 'URL';
+use constant FILENAME => 'FILENAME';
+
+my $websvc = WebService::FileCloud->new( akey => AKEY,
+					 username => USERNAME,
+					 password => PASSWORD );
+
+warn( Dumper( $websvc->fetch_apikey() ) );
+warn( Dumper( $websvc->fetch_account_details() ) );
+warn( Dumper( $websvc->ping() ) );
+warn( Dumper( $websvc->check_file( ukey => UKEY ) ) );
+warn( Dumper( $websvc->fetch_file_details( ukey => UKEY ) ) );
+warn( Dumper( $websvc->fetch_download_url( ukey => UKEY ) ) );
+warn( Dumper( $websvc->fetch_tag_details( tkey => TKEY ) ) );
+warn( Dumper( $websvc->fetch_upload_url() ) );
+warn( Dumper( $websvc->upload_file( url => UPLOAD_URL, filename => FILENAME ) ) );
